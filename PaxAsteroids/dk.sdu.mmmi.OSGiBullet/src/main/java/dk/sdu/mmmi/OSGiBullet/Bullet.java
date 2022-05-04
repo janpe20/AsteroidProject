8e7c216d2716b3c9e2577c74282ffa237edab1ba
Janno <janpe20@student.sdu.dk>
2022-05-04 13:29:59 +0200

    2.2.1
    added most files
    Only one problem at compile

---
+++ PaxAsteroids/dk.sdu.mmmi.OSGiBullet/src/main/java/dk/sdu/mmmi/OSGiBullet/Bullet.java
@@ -0,0 +1,10 @@
+package dk.sdu.mmmi.OSGiBullet;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+
+/**
+ *
+ * @author corfixen
+ */
+public class Bullet extends Entity {
+}
---
+++ PaxAsteroids/dk.sdu.mmmi.OSGiBullet/src/main/java/dk/sdu/mmmi/OSGiBullet/BulletSPI.java
@@ -0,0 +1,12 @@
+package dk.sdu.mmmi.OSGiBullet;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+
+/**
+ *
+ * @author corfixen
+ */
+public interface BulletSPI {
+    Entity createBullet(Entity e, GameData gameData);
+}
